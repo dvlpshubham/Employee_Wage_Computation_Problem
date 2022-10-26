2438e7fb57de5b05ae6d1738429e2e9c22c43c24
Shubham Raj <shubhamaryan805@gmail.com>
2022-10-26 20:17:56 +0530

    added part time employee

--- com/employeewage/EmployeeWageComputation.java
+++ com/employeewage/EmployeeWageComputation.java
@@ -2,13 +2,17 @@ package com.employeewage;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        final int fullTime = 1;
+        final int partTime = 1;
+        final int fullTime = 2;
         final int wagePerHour = 20;
-        int empType = (int) ((Math.random() * 100) % 2);
+        int empType = (int) ((Math.random() * 100) % 3);
         int workingHours = 0;
         if (empType == fullTime) {
-            System.out.println("Employee is present ");
+            System.out.println("Employee is present Full Time ");
             workingHours = 8;
+        } else if (empType == partTime) {
+            System.out.println("Employee is present Part Time");
+            workingHours = 4;
         } else {
             System.out.println("Employee is absent");
         }
