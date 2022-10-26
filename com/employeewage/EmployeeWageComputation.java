dc21304d7c74e3e67fe3c2ec5c628d9afe244c04
Shubham Raj <shubhamaryan805@gmail.com>
2022-10-26 20:09:51 +0530

    added daily wage calculation

--- com/employeewage/EmployeeWageComputation.java
+++ com/employeewage/EmployeeWageComputation.java
@@ -2,15 +2,17 @@ package com.employeewage;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-
         final int fullTime = 1;
+        final int wagePerHour = 20;
         int empType = (int) ((Math.random() * 100) % 2);
-
-        if (empType == fullTime)
+        int workingHours = 0;
+        if (empType == fullTime) {
             System.out.println("Employee is present ");
-
-        else
+            workingHours = 8;
+        } else {
             System.out.println("Employee is absent");
+        }
+        int wage = workingHours * wagePerHour;
+        System.out.println("Employee Daily Wage is :" + wage);
     }
-
 }
