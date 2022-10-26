4ed9e65b9b9948ce06e607e7a603eb110b937651
Shubham Raj <shubhamaryan805@gmail.com>
2022-10-26 20:37:50 +0530

    added calculation of monthly wage

--- com/employeewage/EmployeeWageComputation.java
+++ com/employeewage/EmployeeWageComputation.java
@@ -2,24 +2,32 @@ package com.employeewage;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
+
         final int partTime = 1;
         final int fullTime = 2;
         final int wagePerHour = 20;
-        int empType = (int) ((Math.random() * 100) % 3);
-        int workingHours = 0;
-        switch (empType) {
-            case fullTime:
-                System.out.println("Employee is present Full Time ");
-                workingHours = 8;
-                break;
-            case partTime:
-                System.out.println("Employee is present Part Time");
-                workingHours = 4;
-                break;
-            default:
-                System.out.println("Employee is absent");
+        final int workingDay = 20;
+
+        int monthWage = 0;
+        for (int day = 1; day <= workingDay; day++) {
+            int empType = (int) ((Math.random() * 100) % 3);
+            int workingHours = 0;
+            switch (empType) {
+                case fullTime:
+                    System.out.println("Employee is present Full Time ");
+                    workingHours = 8;
+                    break;
+                case partTime:
+                    System.out.println("Employee is present Part Time");
+                    workingHours = 4;
+                    break;
+                default:
+                    System.out.println("Employee is absent");
+            }
+            int wage = workingHours * wagePerHour;
+            System.out.println("Employee Daily Wage is :" + wage);
+            monthWage += wage;
         }
-        int wage = workingHours * wagePerHour;
-        System.out.println("Employee Daily Wage is :" + wage);
+        System.out.println("Total wage for a month is :" + monthWage);
     }
 }
