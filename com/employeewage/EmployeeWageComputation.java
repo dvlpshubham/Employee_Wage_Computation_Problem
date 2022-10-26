0bd943668d79f06cbfa84b4ff34ca58bbeea3b8f
Shubham Raj <shubhamaryan805@gmail.com>
2022-10-26 21:17:56 +0530

    added wage calculation till max hours or max days are reached

--- com/employeewage/EmployeeWageComputation.java
+++ com/employeewage/EmployeeWageComputation.java
@@ -6,12 +6,17 @@ public class EmployeeWageComputation {
         final int partTime = 1;
         final int fullTime = 2;
         final int wagePerHour = 20;
-        final int workingDay = 20;
+        final int maxWorkingDay = 20;
+        final int maxWorkingHours = 100;
 
         int monthWage = 0;
-        for (int day = 1; day <= workingDay; day++) {
+        int workingHours = 0;
+        int totalWorkingHours = 0;
+        int day;
+        for (day = 1, totalWorkingHours = 0; day <= maxWorkingDay
+                && totalWorkingHours < maxWorkingHours; day++, totalWorkingHours += workingHours) {
             int empType = (int) ((Math.random() * 100) % 3);
-            int workingHours = 0;
+
             switch (empType) {
                 case fullTime:
                     System.out.println("Employee is present Full Time ");
@@ -27,6 +32,8 @@ public class EmployeeWageComputation {
             int wage = workingHours * wagePerHour;
             System.out.println("Employee Daily Wage is :" + wage);
             monthWage += wage;
+            System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHours, wage, totalWorkingHours + workingHours);
         }
         System.out.println("Total wage for a month is :" + monthWage);
     }
